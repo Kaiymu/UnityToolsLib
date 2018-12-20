ebd8f94a0445ec62f3736ed518b80105775426fd
UBISOFT-ORG\tfontaine <tom_1993@hotmail.fr>
2018-12-20 15:35:33 +0100

    [CODE / MOVEMENT]  - Adding basic movement script and new gitignore

---
+++ UnityToolsLib/Assets/Script/Movement/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+    public float playerSpeed;
+    public float dashForce;
+
+    private Rigidbody2D _rig2D;
+
+    private void Awake()
+    {
+        _rig2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate () {
+        Vector2 _direction = new Vector2();
+
+        if (Input.GetKey(KeyCode.Q)) {
+            _direction.x = -transform.right.x;
+        } else if (Input.GetKey(KeyCode.D)) {
+            _direction.x = transform.right.x;
+        }
+
+        if (Input.GetKey(KeyCode.Z)) {
+            _direction.y = transform.up.y;
+        } else if (Input.GetKey(KeyCode.S)) {
+            _direction.y = -transform.up.y;
+        }
+
+        _rig2D.AddForce(_direction.normalized * playerSpeed, ForceMode2D.Force);
+
+        // Dashing
+        if (Input.GetKeyDown(KeyCode.E)) {
+            _rig2D.AddForce(_direction.normalized * dashForce, ForceMode2D.Impulse);
+        }
+    }
+}
